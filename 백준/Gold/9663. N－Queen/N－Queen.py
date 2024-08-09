36f781150ba5c46c6bdffaeb4c89c017b905d391
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-09 16:21:32 +0900

    [Gold IV] Title: N-Queen, Time: 9672 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,8 +1,8 @@
 N = int(input())
 
 col_used = [False] * N
-diagonal_r = [False] * (N * 2 - 1)
-diagonal_l = [False] * (N * 2 - 1)
+diagonal_r = [False] * (N << 1) # /
+diagonal_l = [False] * (N << 1) # \
 result = 0
 
 def get_cnt(row_num:int):
@@ -11,16 +11,21 @@ def get_cnt(row_num:int):
         result += 1
         return
     
-    for i in range(N):
+    for i in range(N if row_num else N >> 1):
         if (col_used[i] 
             or diagonal_r[i + row_num]
-            or diagonal_l[i - row_num + N - 1]) : continue
+            or diagonal_l[row_num - i]) : continue
         
-
-        col_used[i] = diagonal_r[i + row_num] = diagonal_l[i - row_num + N - 1] = True
+        col_used[i] = diagonal_r[i + row_num] = diagonal_l[row_num - i] = True
         get_cnt(row_num + 1)
-        col_used[i] = diagonal_r[i + row_num] = diagonal_l[i - row_num + N - 1] = False
+        col_used[i] = diagonal_r[i + row_num] = diagonal_l[row_num - i] = False
 
 get_cnt(0)
+result <<= 1
+
+if N % 2:
+    mid = N >> 1
+    col_used[mid] = diagonal_l[-mid] = diagonal_r[mid] = True
+    get_cnt(1)
 
 print(result)
