8d3aa1750445dd442a1a64cd18ce8c55952c4d49
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-10 17:32:39 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,38 +1,34 @@
-import sys
-
-result_min = sys.maxsize
+result_min = 10 ** 9
 result_max = -(10 ** 9)
 
 n = int(input())
 
 nums = list(map(int, input().split()))
 oper_cnts = list(map(int, input().split()))
-opers = ['+'] * oper_cnts[0] + ['-'] * oper_cnts[1] + ['*'] * oper_cnts[2] + ['/'] * oper_cnts[3] 
-visit = [False] * len(opers)
 
-def dfs(depth, calc):
-    if depth == n : 
-        global result_max, result_min
-        result_max = max(result_max, calc)
-        result_min = min(result_min, calc)
+def calc(depth, calced):
+    if depth == n:
+        global result_min, result_max
+        result_min = min(result_min, calced)
+        result_max = max(result_max, calced)
         return
     
-    for i in range(len(opers)):
-        if visit[i] : continue
+    for i in range(4):
+        if oper_cnts[i] == 0 : continue
 
-        visit[i] = True
-        dfs(depth+1, calculate(calc, nums[depth], opers[i]))
-        visit[i] = False
+        oper_cnts[i] -= 1
 
-def calculate(num1, num2, oper):
-    if oper == '+' : return num1 + num2
-    elif oper == '-' : return num1 - num2
-    elif oper == '*' : return num1 * num2
+        if i == 0: calc(depth+1, calced + nums[depth])
+        elif i == 1: calc(depth+1, calced - nums[depth])
+        elif i == 2: calc(depth+1, calced * nums[depth])
+        else :
+            if calced * nums[depth] < 0 :
+                calc(depth+1, -(abs(calced) // abs(nums[depth])))
+            else :
+                calc(depth+1, calced // nums[depth])
 
-    # 나눗셈인데 하나만 음수일때
-    if num1 * num2 < 0: return -(abs(num1) // abs(num2))
-    return num1 // num2
+        oper_cnts[i] += 1
 
-dfs(1, nums[0])
+calc(1, nums[0])
 
 print(result_max, result_min, sep='\n')
