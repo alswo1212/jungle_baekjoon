1f927600fee8ba15ee3906edb87ae1f0691ca0b0
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-11 22:09:04 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -2,10 +2,10 @@ A, B, C = map(int, input().split())
 
 def custom_pow(a, b, c):
     if b == 1 : return a % c
-    else :
-        temp = custom_pow(a, b >> 1, c)
+    
+    temp = custom_pow(a, b >> 1, c)
 
-        if b % 2 == 0 : return (temp ** 2) % c
-        else : return (temp ** 2 * a) % c
+    if b % 2 == 0 : return (temp ** 2) % c
+    else : return (temp ** 2 * a) % c
 
 print(custom_pow(A, B, C))
