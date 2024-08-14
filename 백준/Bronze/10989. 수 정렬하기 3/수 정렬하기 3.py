59cdde92b93d4dfa58af8a3612ff5a980ef397b3
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-14 10:36:32 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6712 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,11 +1,13 @@
 import sys
-n = int(sys.stdin.readline())
+N = int(input())
 
-arr = [0] * 10_001
+cnts = [0] * 10_001
 
-for _ in range(n):
-    arr[int(sys.stdin.readline())] += 1
+for _ in range(N):
+    idx = int(sys.stdin.readline())
+    cnts[idx] += 1
 
-for i in range(10_001):
-    if arr[i]:
-        for _ in range(arr[i]) : print(i)
+for i in range(len(cnts)):
+    if cnts[i] == 0 : continue
+    for _ in range(cnts[i]):
+        sys.stdout.write(f'{i}\n')
