6111de36941f204f27f73f7c7904222b4ce34f51
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-19 21:27:55 +0900

    [Gold IV] Title: 탈출, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -6,45 +6,44 @@ R, C = map(int, input().split())
 load = []
 q = []
 visit = []
-
-# 초기 상태 설정
 for i in range(R):
-    row = input().strip()
+    row = input()
     load.append(row)
-    visit.append([0] * C)
-    for j in range(C):
+    visit.append([0] * len(row))
+    for j in range(len(row)):
         if row[j] == 'S':
             heapq.heappush(q, (1, i, j))
-            visit[i][j] = 1  # 고슴도치 방문
+            visit[i][j] = 1
         elif row[j] == '*':
             heapq.heappush(q, (0, i, j))
-            visit[i][j] = 2  # 물 방문
+            visit[i][j] = 2
 
 def BFS(q, load, visit):
+    days = 0
     direc = [(1,0),(-1,0),(0,1),(0,-1)]
     while q:
-        t, y, x = heapq.heappop(q)
-        typ = t % 2  # 물(0)과 고슴도치(1) 구분
+        days += 1
+        for _ in range(len(q)):
+            t, y, x = heapq.heappop(q)
+            typ = t % 2
+            for d in direc:
+                ny = y + d[0]
+                nx = x + d[1]
+                if not(0 <= ny < R) : continue
+                if not(0 <= nx < C) : continue
+                if load[ny][nx] == 'X': continue
 
-        for d in direc:
-            ny = y + d[0]
-            nx = x + d[1]
-            if not (0 <= ny < R and 0 <= nx < C):
-                continue
-            if load[ny][nx] == 'X':  # 벽이면 지나갈 수 없음
-                continue
-            
-            if typ == 0:  # 물 확산
-                if visit[ny][nx] == 0 and load[ny][nx] != 'D':  # 방문하지 않은 곳이면 확산
-                    visit[ny][nx] = 2
-                    heapq.heappush(q, (t + 2, ny, nx))
-            elif typ == 1:  # 고슴도치 이동
-                if load[ny][nx] == 'D':  # 목적지 도착
-                    return t // 2 + 1  # 이동한 일 수 반환
-                if visit[ny][nx] == 0:  # 방문하지 않은 곳
-                    visit[ny][nx] = 1
-                    heapq.heappush(q, (t + 2, ny, nx))
-
+                if typ == 0:
+                    if visit[ny][nx] < 2 and load[ny][nx] != 'D':
+                        visit[ny][nx] = 2
+                        heapq.heappush(q, (t + 2, ny, nx))
+                elif typ == 1:
+                    if load[ny][nx] == 'D':
+                        return days
+                    if visit[ny][nx] == 0:
+                        visit[ny][nx] = 1
+                        heapq.heappush(q, (t + 2, ny, nx))
+                
     return 'KAKTUS'
 
 print(BFS(q, load, visit))
