57763b78d9207c10fc3bc1e84912515d056b7a65
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-19 12:55:44 +0900

    [Gold III] Title: 아침 산책, Time: 708 ms, Memory: 103644 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,28 +1,44 @@
 import sys
-N = int(sys.stdin.readline())
-in_out = sys.stdin.readline()
-links = [[] for _ in range(N)]
-for _ in range(N-1):
-    a, b = map(int, sys.stdin.readline().split())
-    links[a-1].append(b-1)
-    links[b-1].append(a-1)
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-visit = [False] * N
-def dfs(start, now, links, visit, in_out):
-    if start != now and in_out[now] == '1':
-        return 1
-    result = 0
-    for next in links[now]:
-        if visit[next] : continue
-        visit[next] = True
-        result += dfs(start, next, links, visit, in_out)
-        visit[next] = False
-    return result
+n = int(input())
 
-result = 0
-for i in range(N):
-    if in_out[i] == '0': continue
-    visit[i] = True
-    result += dfs(i, i, links, visit, in_out)
-    visit[i] = False
-print(result)
+A = input().rstrip()
+
+graph = [[] for _ in range(n+1)]
+place = [0] * (n+1)
+visited = [0] * (n+1)
+
+for i in range(len(A)):
+    if A[i] == '1':
+        place[i+1] = 1
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def dfs(node):
+    res = 0 
+    for next_node in graph[node]:
+        if place[next_node] == 0:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                res += dfs(next_node)
+        else:
+            res += 1
+    return res
+
+ans = 0
+for i in range(1, n+1):
+    if place[i] == 0:
+        if not visited[i]:
+            visited[i] = 1
+            res = dfs(i)
+            ans += res * (res - 1)
+    else:
+        for next_node in graph[i]:
+            if place[next_node] == 1:
+                ans += 1
+print(ans)
