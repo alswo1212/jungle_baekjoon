125419024d361fcf7b83e33ac911352a80124a56
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-26 20:06:40 +0900

    [Gold IV] Title: 점프, Time: 908 ms, Memory: 67452 KB -BaekjoonHub

---
+++ 백준/Gold/2253. 점프/점프.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(int(1e8))
+N, M = map(int, input().split())
+ignore = set([int(input()) for _ in range(M)])
+MAX = int((2*N) ** 0.5)
+memo = [[sys.maxsize] * (MAX + 2) for _ in range(N+1)]
+
+memo[1][0] = 0 # 1 번 돌에서 시작
+for i in range(2,N+1): # 2번 돌로 점프를 시작
+    if i in ignore:
+        continue
+    for v in range(1, MAX+1):
+        # 해당 속도로 올 수 있는 돌 중에서 가장 작은 횟수 + 1
+        memo[i][v] = min(memo[i-v][v-1],memo[i-v][v],memo[i-v][v+1]) + 1
+
+result = min(memo[N])
+print(-1 if result == sys.maxsize else result)
