b86b5927c526ce386b248732fa90e3aa5eacdc63
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-26 20:12:30 +0900

    [Gold IV] Title: 점프, Time: 548 ms, Memory: 46972 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -9,7 +9,7 @@ memo[1][0] = 0 # 1 번 돌에서 시작
 for i in range(2,N+1): # 2번 돌로 점프를 시작
     if i in ignore:
         continue
-    for v in range(1, MAX+1):
+    for v in range(1, int((2 * i) ** 0.5) + 1):
         # 해당 속도로 올 수 있는 돌 중에서 가장 작은 횟수 + 1
         memo[i][v] = min(memo[i-v][v-1],memo[i-v][v],memo[i-v][v+1]) + 1
 
