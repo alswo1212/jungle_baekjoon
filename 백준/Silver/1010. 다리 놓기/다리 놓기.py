ebc7bb99cc66a1bd916a1b76ba57ff105df4eba6
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-08-29 08:51:53 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,12 +1,8 @@
 n = int(input())
-def calc(n, cnt):
-    if cnt == 0:
-        return 1
-    return n * calc(n-1, cnt-1)
+memo = [1] * (31)
+for i in range(2, 31):
+    memo[i] = memo[i-1] * i
 for _ in range(n):
     a, b = map(int, input().split())
     big, small = max(a,b), min(a,b)
-    memo = [1] * (big + 1)
-    for i in range(2, big+1):
-        memo[i] = memo[i-1] * i
     print(memo[big] // (memo[big-small] * memo[small]))
