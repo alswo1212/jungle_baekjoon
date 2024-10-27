0d1ccca52d911f20cd8095519b65c82f692e1c6d
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-10-27 23:32:54 +0900

    [Gold IV] Title: 가르침, Time: 1864 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def word_2_mask(s:str):
+    result = 0
+    for c in s:
+        result |= 1 << (ord(c) - ord('a'))
+    return result
+
+N, K = map(int, input().strip().split())
+inputs = [word_2_mask(input().strip()) for _ in range(N)]
+base_mask = word_2_mask('antic')
+
+if K < 5:
+    print(0)
+else:
+    removable = [1 << i for i in range(26) if (base_mask & (1 << i)) == 0]
+
+    max_cnt = 0
+    for chars in combinations(removable, K-5):
+        condition_mask = sum(chars) | base_mask
+        cnt = 0
+        for mask in inputs:
+            if mask | condition_mask == condition_mask:
+                cnt += 1
+        max_cnt = max(max_cnt, cnt)
+
+    print(max_cnt)
