ac50c80e3eae83d512c051e985d4b22c56db0525
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2024-10-28 09:43:18 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,25 +1,8 @@
-N, M = map(int,input().split())
-
-nums = list(map(int, input().split()))
-visit = [False] *  len(nums)
-selected = [0] * 3
+from itertools import combinations
+N, M = map(int, input().split())
 result = 0
-
-def make_combi(selected, cnt):
-    if cnt == 3:
-        global result
-        made = sum(selected)
-        if made > result and made <= M: result = made
-        return
-    
-    for i in range(len(nums)):
-        if visit[i] : continue
-
-        visit[i] = True
-        selected[cnt] = nums[i]
-        make_combi(selected, cnt+1)
-        visit[i] = False
-
-make_combi(selected, 0)
-
+for combi in combinations(list(map(int, input().split())), 3):
+    case = sum(combi)
+    if result < case <= M:
+        result = case
 print(result)
