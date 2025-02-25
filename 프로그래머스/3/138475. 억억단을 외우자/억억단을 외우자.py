396b4eb47b4ab5eb604904b0618fa44e961cee3f
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2025-02-25 09:54:52 +0900

    [level 3] Title: 억억단을 외우자, Time: 9504.05 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.py
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.py
@@ -1,10 +1,8 @@
 def solution(e, starts):
-    answer = []
-    nums = [0] * (e+1)
-    for i in range(1, int(e ** 0.5) +1):
-        nums[i*i] += 1
-        for j in range(i+1, e// i + 1):
-            nums[i*j] += 2
+    nums = [1] * (e+1)
+    for i in range(2, e+1):
+        for j in range(i, e+1, i):
+            nums[j] += 1
     
     memo = [0] * (e+1)
     memo[e] = e
@@ -15,7 +13,5 @@ def solution(e, starts):
             max_cnt = nums[i]
         else:
             memo[i] = memo[i+1]
-    for s in starts:
-        answer.append(memo[s])
     
-    return answer
+    return [memo[s] for s in starts]
