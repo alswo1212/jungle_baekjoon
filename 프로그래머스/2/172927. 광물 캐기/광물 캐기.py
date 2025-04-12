8c1d60c0baff10638a36414adc31f418805ad8c0
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2025-04-12 09:33:21 +0900

    [level 2] Title: 광물 캐기, Time: 115.31 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,23 +1,26 @@
 def solution(picks, minerals):
-    answer = 0
-    arr = [i for i in range(len(picks)) for j in range(picks[i])]
-    N, M = len(minerals), len(arr)
-    axes = [
-        {"diamond": 1, 'iron':1, 'stone':1}, 
-        {"diamond": 5, 'iron':1, 'stone':1}, 
-        {"diamond": 25, 'iron':5, 'stone':1}
-        ]
+    n = len(minerals)
+    answer = 25*n
+    info = {"diamond":[1,5,25], "iron":[1,1,5], "stone":[1,1,1]}
 
-    cnt, i = 0, 0
-    bundles = []
-    while cnt < M and i < N:
-        next_i = min(i+5, N)
-        bundles.append(minerals[i:next_i])
-        i = next_i
-        cnt += 1
-    
-    bundles.sort(key=lambda b: sum(axes[2][m] for m in b), reverse=True)
-    for i in range(len(bundles)):
-        answer += sum(axes[arr[i]][m] for m in bundles[i])
+
+    def dfs(idx, cnt, left, current_i):
+        nonlocal n, answer
+        if idx == n or (not idx%5 and left == [0,0,0]):
+            answer = min(answer, cnt)
+            return
+
+        mineral = minerals[idx]
+
+        if idx%5: 
+            dfs(idx+1, cnt + info[mineral][current_i], left, current_i)
+        else:
+            for i in range(3):
+                if not left[i]: continue
+                left[i] -= 1
+                dfs(idx+1, cnt + info[mineral][i], left, i)
+                left[i] += 1
+
+    dfs(0,0,picks,0)
 
     return answer
