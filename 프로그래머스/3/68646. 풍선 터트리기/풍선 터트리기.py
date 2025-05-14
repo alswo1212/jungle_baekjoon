0a231aa46e29ca091beb227bfb16c6c9447535e7
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2025-05-14 16:35:04 +0900

    [level 3] Title: 풍선 터트리기, Time: 974.53 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,15 +1,23 @@
 def solution(a):
-    n = len(a)
-    if n <= 2:
-        return n
-    answer = 2
-    left_min, right_min = [float('inf')]+a[:-1], a[1:]+[float('inf')]
-    for i in range(1, n):
-        left_min[i] = min(left_min[i-1], left_min[i])
-        right_min[n-i-1] = min(right_min[n-i], right_min[n-i-1])
-    
-    for i in range(1, n-1):
-        if a[i] < left_min[i] or a[i] < right_min[i]:
+    answer = 0
+    left, right = float('inf'), float('inf')
+    min_map = [[0, 0] for _ in range(len(a))]
+
+    # 왼쪽 범위에서 최솟값 찾기
+    for i in range(len(a)):
+        if left > a[i]:
+            left = a[i]
+        min_map[i][0] = left
+
+    # 오른쪽 범위에서 최솟값 찾기
+    for i in range(len(a) - 1, -1, -1):
+        if right > a[i]:
+            right = a[i]
+        min_map[i][1] = right
+
+    # 찾은 최솟값 데이터를 가지고 기준 숫자의 양쪽이 모두 기준 숫자보다 작으면 answer += 1
+    for i in range(len(a)):
+        if a[i] <= min_map[i][0] or a[i] <= min_map[i][1]:
             answer += 1
-    
+
     return answer
