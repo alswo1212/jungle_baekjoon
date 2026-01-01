45079d492155119c0f532a0e10d29e9492b4fcf4
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-01-01 15:31:43 +0900

    [Gold V] Title: 토마토, Time: 1120 ms, Memory: 100516 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -6,18 +6,22 @@ for i in range(N):
     for j in range(M):
         if arr[i][j] == 1: q.append((i,j))
 
-result = 0
-direc = [[0,1],[0,-1],[1,0],[-1,0]]
-while q:
-    y, x = q.popleft()
-    days = arr[y][x]
-    for dy, dx in direc:
-        ny, nx = y+dy, x+dx
-        if not(0 <= ny < N and 0 <= nx < M): continue
-        if arr[ny][nx] != 0: continue
-        arr[ny][nx] = days+1
-        result = days
-        q.append((ny,nx))
+def dfs():
+    result = 0
+    direc = [[0,1],[0,-1],[1,0],[-1,0]]
+    while q:
+        y, x = q.popleft()
+        days = arr[y][x]
+        for dy, dx in direc:
+            ny, nx = y+dy, x+dx
+            if not(0 <= ny < N and 0 <= nx < M): continue
+            if arr[ny][nx] != 0: continue
+            arr[ny][nx] = days+1
+            result = days
+            q.append((ny,nx))
+    return result
+
+result = dfs()
 
 for i in range(N):
     for j in range(M):
