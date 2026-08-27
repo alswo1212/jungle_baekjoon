2ea44af9efb3b95b4a83c2e2d5f4c84bccf0be98
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-08-27 23:44:21 +0900

    [level 2] Title: 힌트 스테이지, Time: 71.46 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
@@ -1,3 +1,19 @@
 def solution(cost, hint):
-    answer = 0
-    return answer
+    answer=0
+    hint.append([0,0])
+    N=len(cost)
+    def search(cost_used, now_hints, n):
+        if n==N:
+            return cost_used
+        ncost = cost_used + cost[n][min(now_hints[n], len(cost[n])-1)]
+        rt = search(ncost, now_hints, n+1)
+        hc, *hints = hint[n]
+        ncost+=hc
+        for h in hints:
+            now_hints[h-1]+=1
+        rt=min(rt, search(ncost, now_hints, n+1))
+        for h in hints:
+            now_hints[h-1]-=1
+        return rt
+
+    return search(0, [0]*N,0)
