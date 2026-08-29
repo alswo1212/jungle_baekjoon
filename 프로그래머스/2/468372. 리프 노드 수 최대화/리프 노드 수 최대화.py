8bd96fc32e15dabdbdeb614b3bc1ff9c4566c41b
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-08-29 21:47:17 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 0.07 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.py
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.py
@@ -1,29 +1,35 @@
-def get_leaf_node_count(dist_limit: int, split_limit: int, child_counts: list[int], cur_depth_node_count: int):
+def get_leaf_node_count(
+    dist_limit: int, 
+    split_limit: int, 
+    cur_depth_node_count: int,
+    used_3: bool
+):
     if dist_limit == 0:
         return cur_depth_node_count
     
-    result = 0
+    answer = 0
 
-    for child_count in child_counts:
-        temp_result = 0
+    choose_node_count = dist_limit if cur_depth_node_count >= dist_limit else cur_depth_node_count
+    n_dist_limit = dist_limit - choose_node_count
+    remain_node_count = cur_depth_node_count - choose_node_count
 
-        if split_limit < child_count:
-            temp_result = cur_depth_node_count
-        else:
-            choose_child_count = dist_limit if dist_limit <= cur_depth_node_count else cur_depth_node_count
+    if not used_3 and split_limit >= 2:
+        answer = max(
+            get_leaf_node_count(n_dist_limit, split_limit // 2, choose_node_count * 2, False) + remain_node_count,
+            answer
+        )
 
-            n_dist_limit = dist_limit - choose_child_count
-            n_split_limit = split_limit // child_count
+    if split_limit >= 3:
+        answer = max(
+            get_leaf_node_count(n_dist_limit, split_limit // 3, choose_node_count * 3, True) + remain_node_count,
+            answer
+        )
 
-            n_depth_node_count = choose_child_count * child_count
-            temp_result = get_leaf_node_count(n_dist_limit, n_split_limit, child_counts, n_depth_node_count) 
-            temp_result += cur_depth_node_count - choose_child_count
+    if split_limit < 2:
+        answer += cur_depth_node_count
 
+    return answer
 
-        result = max(result, temp_result)
-
-    return result
 
 def solution(dist_limit, split_limit):
-    child_counts = [2, 3]
-    return get_leaf_node_count(dist_limit, split_limit, child_counts, 1)
+    return get_leaf_node_count(dist_limit, split_limit, 1, False)
