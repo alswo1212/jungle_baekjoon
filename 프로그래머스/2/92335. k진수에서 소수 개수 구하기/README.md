d05db989715e29700ba1afbeab6c6fe46e3c5d77
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-09-12 09:19:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 15.65 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 49.89 ms
+메모리: 11.2 MB, 시간: 15.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 08일 19:39:29
+2026년 09월 12일 09:19:33
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,24 +1,22 @@
+from math import sqrt
+
+def is_prime(num:int):
+    if num < 2 : return False
+    if num <= 3 : return True
+    for i in range(3, int(sqrt(num))+1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def to_int(s:str): return int(s) if s.isnumeric() else 0
+
 def solution(n, k):
-    def is_prime(num:int)->bool:
-        if num < 2:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, int(num**0.5) + 1, 2):
-            if num % i == 0:
-                return False
-        return True
-    
-    answer = 0
-    nums = []
+    char_stack = []
+    k_nums = ''
     while n:
-        nums.append(str(n % k))
+        char_stack.append(str(n%k))
         n //= k
-    for num in ''.join(nums[::-1]).split('0'):
-        if num == '':
-            continue
-        if is_prime(int(num)):
-            answer += 1
-    return answer
+    while char_stack:
+        k_nums += char_stack.pop()
+    
+    return len(list(filter(is_prime,map(to_int, k_nums.split('0')))))
