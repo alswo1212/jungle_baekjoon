4fbd502a5aef81a50f1444709128885abe08d11c
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-09-17 16:32:11 +0900

    [level 2] Title: 순위 검색, Time: 393.85 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,44 +1,27 @@
 from bisect import bisect_left
-def make_all_cases():
-    options = [
-        ["cpp", "java", "python", "-"],
-        ["backend", "frontend", "-"],
-        ["junior", "senior", "-"],
-        ["chicken", "pizza", "-"],
-    ]
-    all_cases = []
-    def make_case(idx:int,result:str):
-        if idx == len(options):
-            all_cases.append(result)
-            return
-        for word in options[idx]:
-            make_case(idx+1, result + word)
-    make_case(0, '')
-    
-    return all_cases
-    
-def solution(info:list[str], query:list[str]):
+from collections import defaultdict
+from itertools import product
+
+def solution(info, query):
+    score_table = defaultdict(list)
+
+    for entry in info:
+        language, job, career, food, score = entry.split()
+        score = int(score)
+
+        for key in product(*[(value, "-") for value in (language, job, career, food)]):
+            score_table[key].append(score)
+
+    for scores in score_table.values():
+        scores.sort()
+
     answer = []
-    db = { key: [] for key in make_all_cases()}
-    
-    for selects in info:
-        lang, dev, career, food, score = selects.split()
-        score = int(score)
-        options = [lang, dev, career, food]
-        for bits in range(1 << len(options)):
-            db_key = ''
-            for i in range(len(options)):
-                db_key += options[i] if bits % 2 else '-'
-                bits >>= 1
-            db[db_key].append(score)
-            
-    for values in db.values(): values.sort()
-    
     for q in query:
-        lang, _, dev, _, career, _, food, score = q.split()
-        score = int(score)
-        db_key = lang + dev + career + food
-        idx = bisect_left(db[db_key], score)
-        answer.append(len(db[db_key]) - idx)
-        
+        tokens = q.split()
+        language, job, career, food = tokens[0], tokens[2], tokens[4], tokens[6]
+        target_score = int(tokens[7])
+
+        scores = score_table[(language, job, career, food)]
+        answer.append(len(scores) - bisect_left(scores, target_score))
+
     return answer
