d80f0142519f29f302e902e0a544b0d9e29744b0
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-09-17 22:02:00 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.22 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 18.54 ms
+메모리: 11.8 MB, 시간: 1.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 11일 13:06:25
+2026년 09월 17일 22:01:56
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,38 +1,24 @@
+from itertools import combinations
 from collections import defaultdict
-def solution(orders, course):
-    answer = []
-    want_lens = set(course)
-    counter = defaultdict(int)
-    for order in orders:
-        order = sorted(order)
-        for bits in range(1 << len(order)):
-            bit_cnt = 0
-            set_menu = ''
-            i = 0
-            while bits:
-                if bits%2:
-                    set_menu += order[i]
-                    bit_cnt += 1
-                bits >>= 1
-                i+=1
-            if bit_cnt not in want_lens:
-                continue
-            counter[set_menu] += 1
-    
-    popular_set = defaultdict(list)
-    max_cnt = defaultdict(int)
-    for set_menu, cnt  in counter.items():
-        if cnt < 2:
-            continue
-        
-        n = len(set_menu)
-        if max_cnt[n] < cnt:
-            popular_set[n] = [set_menu]
-            max_cnt[n] = cnt
-        elif max_cnt[n] == cnt:
-            popular_set[n].append(set_menu)
-    
-    for arr in popular_set.values(): answer.extend(arr)
-    answer.sort()
 
+def solution(orders:list[str], course:list[int]):
+    answer = []
+    for length in course:
+        counter = defaultdict(int)
+        for order in orders:
+            for menus in combinations(sorted(order), length):
+                counter[menus] += 1
+                
+        max_count = 2
+        temp = []
+        for menus, count in counter.items():
+            if count > max_count:
+                temp.clear()
+                temp.append(''.join(menus))
+                max_count = count
+            elif count == max_count:
+                temp.append(''.join(menus))
+        answer += temp
+        
+    answer.sort()
     return answer
