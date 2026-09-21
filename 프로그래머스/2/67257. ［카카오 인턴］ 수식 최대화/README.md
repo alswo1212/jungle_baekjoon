140999eb2e49db3e9539786e89bc8567271d5c26
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-09-21 17:09:50 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.08 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/README.md
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/README.md
@@ -1,10 +1,10 @@
 # [level 2] [카카오 인턴] 수식 최대화 - 67257 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257) 
 
 ### 성능 요약
 
-메모리: 9.4 MB, 시간: 0.20 ms
+메모리: 11.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 14:38:50
+2026년 09월 21일 17:09:46
 
 ### 문제 설명
 
--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,36 +1,37 @@
 from itertools import permutations
-def solution(expression:str):
-    def calc(num1:int, num2:int, oper:str)->int:
-        if oper == '*':
-            return num1 * num2
-        elif oper == '+':
-            return num1 + num2
-        elif oper == '-':
-            return num1 - num2
-    
-    answer = 0
-    inputs, opers = [], []
-    left = 0
-    for i in range(1,len(expression)):
-        if not expression[i].isdigit():
-            inputs.append(int(expression[left:i]))
-            opers.append(expression[i])
-            left = i+1
-    inputs.append(int(expression[left:]))
-    
-    use_oper = set(opers)
-    for purmut in permutations(use_oper, len(use_oper)):
-        c_input = [*inputs]
-        c_opers = [*opers]
-        for oper in purmut:
-            i = 0
-            while i < len(c_input)-1:
-                if c_opers[i] == oper:
-                    c_opers[i:i+1] = []
-                    c_input[i:i+2] = [calc(c_input[i],c_input[i+1],oper)]
-                    continue
-                i+=1
-        if abs(c_input[0]) > answer:
-            answer = abs(c_input[0])
-        
+from collections import deque
+
+def str_2_list(expression:str):
+    answer = []
+    idx = 0
+    for i, c in enumerate(expression):
+        if not c.isnumeric():
+            answer.append(int(expression[idx:i]))
+            answer.append(c)
+            idx = i+1
+    answer.append(int(expression[idx:]))
     return answer
+
+def calc(exp_list:list[int], order: list[str]):
+    q1 = deque(exp_list)
+    q2 = deque()
+    for o in order:
+        target_q = q1 if q1 else q2
+        other_q =  q2 if q1 else q1
+        while target_q:
+            word = target_q.popleft()
+            if o == word:
+                if o == '*':
+                    other_q.append(other_q.pop() * target_q.popleft())
+                elif o == '+':
+                    other_q.append(other_q.pop() + target_q.popleft())
+                else:
+                    other_q.append(other_q.pop() - target_q.popleft())
+            else:
+                other_q.append(word)
+    
+    return  abs(q1[0] if q1 else q2[0])
+
+def solution(expression:str):
+    exp_list = str_2_list(expression)
+    return max(calc(exp_list, order) for order in permutations("+-*", 3))
