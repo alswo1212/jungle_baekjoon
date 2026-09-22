be3723450735f7a0b25cca4507976cd3900967b4
alswo1212 <92290312+alswo1212@users.noreply.github.com>
2026-09-22 19:57:41 +0900

    [level 2] Title: 튜플, Time: 9.59 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,4 +1,13 @@
-from collections import Counter
-import re
-def solution(s):
-    return list(map(lambda x: int(x[0]),Counter(re.findall('\d+', s)).most_common()))
+def solution(s:str):
+    answer = []
+    arr = s.removeprefix("{{").removesuffix("}}").split('},{')
+    arr.sort(key=lambda ar: len(ar))
+    already = set()
+    for ss in arr:
+        sub_arr = list(map(int, ss.split(',')))
+        for num in sub_arr:
+            if num not in already:
+                answer.append(num)
+                already.add(num)
+                break
+    return answer
